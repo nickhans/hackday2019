ed87fd5e20752fdc43b2d31368518517cca753ac
Jacob <jabo9820@colorado.edu>
2019-12-07 13:11:59 -0700

    Updating Settings and Migrations

--- hackday/settings.py
+++ hackday/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'felony_football.apps.FelonyFootballConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
