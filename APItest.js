86f1f3f762493be4f70c3c36e3d3febcf5eacc6d
Adam Bender <adbe1633@colorado.edu>
2019-12-07 15:50:54 -0700

    upated APItest.js to include API calls for producing top 5 players and top 5 crimes

--- APItest.js
+++ APItest.js
@@ -24,18 +24,20 @@ var HttpClient = function() {
     }
 }
 
+startYear = "2000"
+endYear = "2019"
 
-start_date = "2019-01-01"
-end_date = "2019-12-31"
+start_date = startYear + "-01-01"
+end_date = endYear + "-12-31"
 
-
-// var teamID = league[key]
 
 var client = new HttpClient();
+
+//Get league teams ranked by number of arrests for league screen
 client.get('https://NflArrest.com/api/v1/team?start_date=' + start_date + '&end_date=' + end_date, function(response) {
     // do something with response
     var dataObject = JSON.parse(response)
-    console.log(dataObject)
+    // console.log(dataObject)
     for(i = 0; i < dataObject.length; i++){
         for(var user in league){
             // console.log(league[user])
@@ -45,9 +47,34 @@ client.get('https://NflArrest.com/api/v1/team?start_date=' + start_date + '&end_
             }
         }
     }
+});
+
+
+teamID = "DEN"
+limit = "5"
+
+//return top 5 players from Broncos (teamID) for individual user team screen
+client.get('https://NflArrest.com/api/v1/team/topPlayers/' + teamID + '?start_date=' + start_date + '&end_date=' + end_date + '&limit=' + limit, function(response) {
+    // do something with response
+    var dataObject = JSON.parse(response)
+    // console.log(dataObject)
+    for(i = 0; i < dataObject.length; i++){
+        returnString = "Player Name:  " + dataObject[i]["Name"] + ", No. of Arrests: " + dataObject[i]["arrest_count"]
+        console.log(returnString)
+    }
 
 });
-// console.log('\n')
+
+//return top 5 crimes from Broncos (teamID) for individual user team screen
+client.get('https://NflArrest.com/api/v1/team/topCrimes/' + teamID + '?start_date=' + start_date + '&end_date=' + end_date + '&limit=' + limit, function(response) {
+    // do something with response
+    var dataObject = JSON.parse(response)
+    // console.log(dataObject)
+    for(i = 0; i < dataObject.length; i++){
+        returnString = "Crime:  " + dataObject[i]["Category"] + ", No. of Arrests: " + dataObject[i]["arrest_count"]
+        console.log(returnString)
+    }
+});
 
 
 
