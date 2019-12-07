9c824329ec6e181efa2b8f023352ee7afa0a7fe4
Nick Hanselman <nickhanselman@icloud.com>
2019-12-07 13:56:15 -0700

    register models to admin

--- felony_football/admin.py
+++ felony_football/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from felony_football.models import Team, League, Profile, Season
 
 # Register your models here.
+admin.site.register(Team)
+admin.site.register(League)
+admin.site.register(Profile)
+admin.site.register(Season)
